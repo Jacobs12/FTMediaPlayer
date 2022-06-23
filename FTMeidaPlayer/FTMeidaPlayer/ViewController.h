11f080d55767dfc23a9e237715bb65bb7f39c1ec
fengtao92 <fengtao@ewang.com>
2022-06-23 10:36:06 +0800

    建立视频播放器框架

---
+++ FTMeidaPlayer/FTMeidaPlayer/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  FTMeidaPlayer
+//
+//  Created by wolffy on 2022/6/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FTMeidaPlayer/FTMeidaPlayer/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  FTMeidaPlayer
+//
+//  Created by wolffy on 2022/6/23.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
